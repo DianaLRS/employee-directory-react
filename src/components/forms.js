64d031a125a93a789f0dd5846a93cce93699073b
Luna <dlramos194@gmail.com>
2020-11-15 17:54:58 -0600

    configuring files. importing files. header added

---
+++ src/components/forms.js
@@ -0,0 +1,14 @@
+
+// function Form() {
+
+//     return(
+// <nav classname="navbar navbar-light bg-light">
+//   <form classname="form-inline">
+//     <input classname="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+//     <button classname="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//   </form>
+// </nav>
+
+//     )
+// }
+
