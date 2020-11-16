642c6e2bdab3ca2846815a7a99aed2d56fad4f41
Luna <dlramos194@gmail.com>
2020-11-15 19:42:29 -0600

    set up form

---
+++ src/components/SearchForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SearchForm.css";
+
+
+function SearchForm(){
+return(
+  <>
+
+<input classname="form-control" type="text" placeholder="Search Directory"></input>
+</>
+)
+
+  }
+
+
+
+
+export default SearchForm;
